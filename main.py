dff94b8cc7b4978437b8a4423c6a0388db246268
Yoav <ykaliblotzky@ucdavis.edu>
2018-06-05 16:05:06 -0700

    Initialized main

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import pandas as pd
 
-X = pd.read_csv('aggregate_data.csv')
+data = pd.read_csv('aggregate_data.csv')
+X = data[[x for x in data if x != 'result']]
+for x in X:
+    X[x] = (X[x] - min(X[x]))/(max(X[x] - min(X[x])))
+
+y = data[['result']]
+
+
+
