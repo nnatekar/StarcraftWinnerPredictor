c0e1a13f3417d14afc51503e7c4ae3fe684ba773
Neil Natekar <neilnatekar@gmail.com>
2018-06-05 17:21:02 -0700

    accidentally committed wrong main.py last time

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pandas as pd
 
-data = pd.read_csv('aggregate_data.csv')
+data = pd.read_csv('data/aggregate_data.csv')
 X = data[[x for x in data if x != 'result']]
 for x in X:
     X[x] = (X[x] - min(X[x]))/(max(X[x] - min(X[x])))
 
 y = data[['result']]
 
+netGenerator = NetworkGenerator()
+networks = [None] * 10
+for i in range(0, 10):
+    networks[i] = netGenerator.generate()
 
+genetic = Genetic(networks)
 
+endNetworks = genetic.begin(X, y)
