e1b00fdd93c3a3ae55f89bb8f9792fd03bada15d
yoavkali <ykaliblotzky@ucdavis.edu>
2018-06-08 03:59:49 +0000

    fixed small error

--- evaluate_fitnesses.py
+++ evaluate_fitnesses.py
@@ -6,10 +6,10 @@ data = pd.read_csv('./data/aggregate_data.csv')
 X = data[[x for x in data if x != 'result']]
 for x in X:
     X[x] = (X[x] - min(X[x]))/(max(X[x] - min(X[x])))
-if len(sys.argv) == 1:
+if len(sys.argv) == 2:
     y = data[['result']]
     for i in range(10):
-        with open('network{}.pickle'.format(int(sys.argv[0]) + i), 'rb') as handle:
+        with open('network{}.pickle'.format(int(sys.argv[1]) + i), 'rb') as handle:
             n = pickle.load(handle)
 
         print(evaluate_fitness(n, X, y))
